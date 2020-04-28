9805aef6d314a93bb621280f53cf2490eed766a5
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-04-28 23:51:20 +0300

    integrate redux provider

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+import * as React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import reducers from './src/redux/reducers';
+import thunk from 'redux-thunk';
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(reducers, storeEnhancers(applyMiddleware(thunk)));
+
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
