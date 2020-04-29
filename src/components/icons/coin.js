b781b3e7ff9803ed711e5ac98cb95bfd88b79a4f
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-04-29 13:15:30 +0300

    edited icons

--- src/components/icons/coin.js
+++ src/components/icons/coin.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import {Svg, Path} from 'react-native-svg';
+
 const CoinIcon = (props) => (
-  <svg fill="none" {...props}>
-    <path
+  <Svg fill="#FFCB45" {...props} width={25} height={22}>
+    <Path
       d="M10 20C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 100-16.001A8 8 0 0010 18zm0-12.95L14.95 10 10 14.95 5.05 10 10 5.05zm0 2.829L7.879 10 10 12.121 12.121 10 10 7.879z"
       fill="#FFCB45"
     />
-  </svg>
+  </Svg>
 );
 
 export default CoinIcon;
--- src/components/icons/exit.js
+++ src/components/icons/exit.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Svg, Path} from 'react-native-svg';
 
 const ExitIcon = (props) => (
-  <svg fill="none" {...props}>
-    <path
+  <Svg fill="none" {...props} width={25} height={22}>
+    <Path
       d="M12.59 6L10 8.59 7.41 6 6 7.41 8.59 10 6 12.59 7.41 14 10 11.41 12.59 14 14 12.59 11.41 10 14 7.41 12.59 6zM10 0C4.47 0 0 4.47 0 10s4.47 10 10 10 10-4.47 10-10S15.53 0 10 0zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
       fill="#000"
     />
-  </svg>
+  </Svg>
 );
 
 export default ExitIcon;
