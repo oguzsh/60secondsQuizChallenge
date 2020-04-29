51b51f835a97cee5d8bf82a052edcdc729552d35
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-04-29 13:15:58 +0300

    edited views

--- src/views/CorrectAnswerView.js
+++ src/views/CorrectAnswerView.js
@@ -1,21 +1,62 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 
-import Row from '../components/base/Row';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchQuestions} from '../redux/actions';
+
 import Column from '../components/base/Column';
+import Row from '../components/base/Row';
 import Text from '../components/base/Text';
 import ActionButton, {ActionButtonTitle} from '../components/ActionButton';
+import CorrectLogo from '../components/CorrectLogo';
+
+import theme from '../utils/theme';
+import Score from '../components/Score';
 
 const CorrectAnswerView = ({navigation}) => {
   return (
-    <Row as={SafeAreaView} flex={1}>
-      <Column>
-        <Text> Correct Answer Page </Text>
-        <ActionButton>
-          <ActionButtonTitle>NEXT</ActionButtonTitle>
+    <Column as={SafeAreaView} flex={1} bg="white">
+      <Column mt={40}>
+        <CorrectLogo />
+      </Column>
+
+      <Column flex={1}>
+        <Text fontFamily={theme.fontFamily.bold} color="black" fontSize={28}>
+          Congratulations!
+        </Text>
+
+        <Row>
+          <Text
+            mr={10}
+            fontFamily={theme.fontFamily.medium}
+            color="black"
+            fontSize={18}>
+            You're won
+          </Text>
+          <Score score={100} />
+        </Row>
+
+        <Row mt={20}>
+          <Text
+            mr={10}
+            fontFamily={theme.fontFamily.medium}
+            color="black"
+            fontSize={18}>
+            Total:
+          </Text>
+          <Score score={800} />
+        </Row>
+
+        <ActionButton
+          mt={20}
+          borderRadius={30}
+          onPress={() => navigation.navigate("WrongAnswer")}
+          bg="purple">
+          <ActionButtonTitle color="white">NEXT QUESTION</ActionButtonTitle>
         </ActionButton>
       </Column>
-    </Row>
+    </Column>
   );
 };
 
--- src/views/WrongAnswerView.js
+++ src/views/WrongAnswerView.js
@@ -1,21 +1,63 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 
-import Row from '../components/base/Row';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchQuestions} from '../redux/actions';
+
 import Column from '../components/base/Column';
+import Row from '../components/base/Row';
 import Text from '../components/base/Text';
 import ActionButton, {ActionButtonTitle} from '../components/ActionButton';
+import WrongLogo from '../components/WrongLogo';
+
+import theme from '../utils/theme';
+import Score from '../components/Score';
 
 const WrongAnswerView = ({navigation}) => {
   return (
-    <Row as={SafeAreaView} flex={1}>
-      <Column>
-        <Text> Wrong Answer Page </Text>
-        <ActionButton>
-          <ActionButtonTitle>PLAY AGAIN</ActionButtonTitle>
+    <Column as={SafeAreaView} flex={1} bg="white">
+      <Column mt={40}>
+        <WrongLogo />
+      </Column>
+
+      <Column flex={1}>
+        <Text fontFamily={theme.fontFamily.bold} color="black" fontSize={28}>
+          Game Over!
+        </Text>
+
+        <Text fontFamily={theme.fontFamily.bold} color="black" fontSize={18}>
+          Maybe next time :(
+        </Text>
+
+        <Row mt={20}>
+          <Text
+            mr={10}
+            fontFamily={theme.fontFamily.medium}
+            color="black"
+            fontSize={18}>
+            You Score:
+          </Text>
+          <Score score={800} />
+        </Row>
+
+        <ActionButton
+          mt={30}
+          borderRadius={30}
+          onPress={() => console.log('hi')}
+          bg="green">
+          <ActionButtonTitle color="white">PLAY AGAIN</ActionButtonTitle>
+        </ActionButton>
+
+        <ActionButton
+          mt={20}
+          borderRadius={30}
+          onPress={() => console.log('hi')}
+          bg="black">
+          <ActionButtonTitle color="white">RETURN HOME</ActionButtonTitle>
         </ActionButton>
       </Column>
-    </Row>
+    </Column>
   );
 };
 
