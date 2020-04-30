c4eff31d59661e85eaf9f29224b4e6ea6d357b4d
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-04-30 10:27:42 +0300

    added default parameters

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,12 +1,16 @@
-export const fetchData = async ({amount, difficulty, category}) => {
-  const URL = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`;
+export const getData = async (
+  amount = '10',
+  difficulty = 'easy',
+  category = '9',
+) => {
+  const URL = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&category=${category}&type=multiple`;
 
   let response = await fetch(URL);
   let data = await response.json();
   return data;
 };
 
-export const fetchAllCategories = async () => {
+export const getAllCategories = async () => {
   const URL = 'https://opentdb.com/api_category.php';
 
   let response = await fetch(URL);
