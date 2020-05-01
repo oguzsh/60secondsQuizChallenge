379b892d6d2ee97255089bbc836318980bdf3b70
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-05-01 05:00:35 +0300

    add fetchQuestions and edited components

--- src/views/QuestionView.js
+++ src/views/QuestionView.js
@@ -1,31 +1,121 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+import React, {useState, useLayoutEffect} from 'react';
+import {SafeAreaView, Dimensions, ActivityIndicator} from 'react-native';
+import {connect} from 'react-redux';
+import * as actions from '../redux/actions';
 
 import Row from '../components/base/Row';
 import Column from '../components/base/Column';
-import Text from '../components/base/Text';
-import AnswerButton, {AnswerButtonTitle} from '../components/AnswerButton';
 
-const QuestionView = ({navigation}) => {
+import ProgressBar from '../components/ProgressBar';
+import Score from '../components/Score';
+import QuestionTracker from '../components/QuestionTracker';
+import Question from '../components/Question';
+
+const QuestionView = ({
+  currentQuestionNumber,
+  currentQuestion,
+  currentQuestionIndex,
+  questions,
+  totalQuestionsSize,
+  totalScore,
+  fetchQuestions,
+  loading,
+  gameOver,
+  nextQuestion,
+  selectedCategoryId,
+  selectedDifficulty,
+  time,
+  navigation,
+}) => {
+  const {width} = Dimensions.get('window');
+  const [state, setState] = useState({
+    type: '',
+  });
+
+  const handleAnswerSelection = (questionChoice) => {
+    const type =
+      questionChoice === null
+        ? 'timeout'
+        : questionChoice === currentQuestion.correct_answer
+        ? 'correct'
+        : 'incorrect';
+
+    setState({type: type});
+
+    setTimeout(() => {
+      nextQuestion(
+        questionChoice,
+        currentQuestionIndex,
+        questions,
+        type,
+        navigation,
+        totalScore + 100,
+      );
+    }, 500);
+  };
+
+  useLayoutEffect(() => {
+    fetchQuestions(selectedDifficulty, selectedCategoryId.value);
+  }, [fetchQuestions, selectedCategoryId.value, selectedDifficulty]);
+
   return (
-    <Row as={SafeAreaView} flex={1}>
-      <Column>
-        <Text> Question Page </Text>
-        <AnswerButton m={10}>
-          <AnswerButtonTitle>A</AnswerButtonTitle>
-        </AnswerButton>
-        <AnswerButton m={10}>
-          <AnswerButtonTitle>B</AnswerButtonTitle>
-        </AnswerButton>
-        <AnswerButton m={10}>
-          <AnswerButtonTitle>C</AnswerButtonTitle>
-        </AnswerButton>
-        <AnswerButton m={10}>
-          <AnswerButtonTitle>D</AnswerButtonTitle>
-        </AnswerButton>
-      </Column>
-    </Row>
+    <Column flex={1} bg="white" as={SafeAreaView}>
+      {gameOver ? navigation.navigate('WrongAnswer') : null}
+
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Column>
+          <Row justifyContent="space-around" width={width}>
+            <QuestionTracker
+              currentQuestion={currentQuestionNumber}
+              amountQuestions={totalQuestionsSize}
+            />
+            <ProgressBar time={time} navigation={navigation} />
+            <Score score={totalScore} />
+          </Row>
+
+          <Question
+            question={currentQuestion.question}
+            choices={currentQuestion.choices}
+            onItemSelected={handleAnswerSelection}
+            status={state.type}
+          />
+        </Column>
+      )}
+    </Column>
   );
 };
 
-export default QuestionView;
+const mapStateToProps = ({questionsReducer}) => {
+  const {
+    currentQuestionIndex,
+    error,
+    loading,
+    selectedCategoryId,
+    questions,
+    totalScore,
+    amountQuestions,
+    selectedDifficulty,
+    gameOver,
+    time,
+  } = questionsReducer;
+
+  return {
+    currentQuestion: questions[currentQuestionIndex],
+    currentQuestionNumber: currentQuestionIndex + 1,
+    totalQuestionsSize: questions.length,
+    currentQuestionIndex,
+    error,
+    time,
+    selectedCategoryId,
+    loading,
+    selectedDifficulty,
+    gameOver,
+    amountQuestions,
+    questions,
+    totalScore,
+  };
+};
+
+export default connect(mapStateToProps, actions)(QuestionView);
