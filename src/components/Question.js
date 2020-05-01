19ea94a01b06877b4a5f1c8ecf78ccf88f998007
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-05-01 04:58:59 +0300

    edited Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -9,31 +9,27 @@ import Divider from './base/Divider';
 function Question({question, choices, onItemSelected, status}) {
   const {width} = Dimensions.get('window');
 
- /* const buttonStyle =
-    status === 'correct'
-      ? {backgroundColor: 'green'}
-      : status === 'incorrect'
-      ? {backgroundColor: 'red'}
-      : null;
-*/
-
   return (
     <Column>
-      <Text m={30} fontFamily="Poppins-Medium" fontSize={18} color="black">
-        {question}
-      </Text>
-      <Divider width={width - 50} />
-      <Column mt={30}>
-        {choices.map((choice) => {
-          return (
-            <AnswerButton
-              key={choice.id}
-              m={10}
-              onPress={() => onItemSelected(choice)}>
-              <AnswerButtonTitle>{choice}</AnswerButtonTitle>
-            </AnswerButton>
-          );
-        })}
+      <Column>
+        <Text m={30} fontFamily="Poppins-Medium" fontSize={18} color="black">
+          {question.question}
+        </Text>
+        <Divider width={width - 50} />
+        <Column mt={30}>
+          {choices.map((choice) => {
+            return (
+              <AnswerButton
+                key={choice.id}
+                m={10}
+                onPress={() => {
+                  onItemSelected(choice);
+                }}>
+                <AnswerButtonTitle>{choice}</AnswerButtonTitle>
+              </AnswerButton>
+            );
+          })}
+        </Column>
       </Column>
     </Column>
   );
