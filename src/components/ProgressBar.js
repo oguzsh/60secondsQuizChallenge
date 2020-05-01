4bfc504f5efbfcb91a829a45160b8613869891d0
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-05-01 04:59:48 +0300

    edited progressBar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import AnimatedProgressWheel from 'react-native-progress-wheel';
 
 import theme from '../utils/theme';
 import Text from './base/Text';
+import Column from './base/Column';
 
-const ProgressBar = ({progress = 50}) => {
+const ProgressBar = ({time, navigation}) => {
+  const [timer, setTimer] = useState(time);
+
+  useEffect(() => {
+    let interval = null;
+
+    interval = setInterval(() => {
+      if (timer <= 0) {
+        navigation.navigate('WrongAnswer');
+        clearInterval(interval);
+      } else {
+        setTimer(timer - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timer]);
+
   return (
-    <AnimatedProgressWheel
-      size={64}
-      width={8}
-      color={theme.colors.purple}
-      progress={80}
-      animateFromValue={0}
-      duration={2000}
-      backgroundColor={theme.colors.lightGrey}
-    >
-    </AnimatedProgressWheel>
+    <Column>
+      <AnimatedProgressWheel
+        size={64}
+        width={6}
+        color={theme.colors.purple}
+        progress={100}
+        animateFromValue={0}
+        duration={60000}
+        backgroundColor={theme.colors.lightGrey}
+      />
+
+      <Text
+        style={{position: 'absolute', top: 20}}
+        fontFamily="Poppins-Bold"
+        fontSize={18}>
+        {timer}
+      </Text>
+    </Column>
   );
 };
 
